b48a07255a2cd717a7666f644b1f62358d8b135a
prof-scialpi <55781621+itismeucci-fi@users.noreply.github.com>
2021-10-20 12:17:20 +0200

    modifica casa

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Lezione GIT");
         System.out.println("Ancora una volta!!!!");
+        System.out.println("Modifica fatta in laboratorio");
     }
 }
